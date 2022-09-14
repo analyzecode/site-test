16179c3771947d746d3fda82d08f32a57e59fc1d
analyzecode <analyzecode11@gmail.com>
2022-09-14 18:54:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,6 @@ var themeSwitch = document.getElementById("switch-theme");
 var bannerBlob = document.querySelector(".banner-blob");
 
 themeSwitch.onclick = function(){
-//  document.body.classList.toggle("dark-theme-colors")
-//     if(document.body.classList.contains("dark-theme-colors")){
-//         themeSwitch.classList.remove("bx-moon");
-//         themeSwitch.classList.add("bx-sun");
-//         bannerBlob.setAttribute("style","display: none;");
-//     }
-//     else{
-//         themeSwitch.classList.remove("bx-sun");
-//         themeSwitch.classList.add("bx-moon");
-//         bannerBlob.setAttribute("style","display: initial;");
-
-//     }
 document.body.classList.toggle("dark-theme-colors")
 if(document.body.classList.contains("dark-theme-colors")){
 
